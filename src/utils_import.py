8df216c986cde5eb4bef9bf06b5a8668b727b2ec
Joel <joelrubiogonz@gmail.com>
2024-11-16 17:33:27 +0100

    Modified load_data

--- src/utils_import.py
+++ src/utils_import.py
@@ -20,16 +20,22 @@ def cache_data(func):
 
     return wrapper
 
-@cache_data
-def load_data(input_directory:str, metadata:str) -> dict:
-    metadata_pd = pd.read_csv(metadata, skip_blank_lines = True, header = 0)
-    FileName = list(metadata_pd["FileName"])
-    _list = []
+def scan_directory(_list:list, input_directory:str, metadata:pd.DataFrame):
+    FileName = list(metadata["FileName"])
     for arch in tqdm(os.scandir(input_directory)): 
         if arch.name in FileName:
             with open(arch.path, "rb") as file:
                 signal = np.frombuffer(file.read(), dtype=np.complex64)
-                row = metadata_pd.loc[metadata_pd["FileName"] == arch.name].index[0]
-                _dict = {x:y for x,y in [("Data",signal),("Class",metadata_pd["SignalType"][row]),("JammingStartTime",metadata_pd["JammingStartTime"][row]),("AveragePower_dB",metadata_pd["AveragePower_dB"][row])]}
+                row = metadata.loc[metadata["FileName"] == arch.name].index[0]
+                _dict = {x:y for x,y in [("Data",signal),("Class",metadata["SignalType"][row]),("JammingStartTime",metadata["JammingStartTime"][row]),("AveragePower_dB",metadata["AveragePower_dB"][row])]}
                 _list.append(_dict)
+        if os.path.isdir(arch) == True:
+            _list = scan_directory(_list,arch.path,metadata)
+    return _list
+
+@cache_data
+def load_data(input_directory:str, metadata:str) -> list:
+    metadata_pd = pd.read_csv(metadata, skip_blank_lines = True, header = 0)
+    _list = []
+    _list = scan_directory(_list,input_directory,metadata_pd)  
     return _list
