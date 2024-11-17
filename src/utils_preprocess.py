e7e90afb0c41984a889f664b0d7557b67843dbb5
stelin41 <stelin41.io@gmail.com>
2024-11-17 17:18:52 +0100

    Attempt to fix labels

--- src/utils_preprocess.py
+++ src/utils_preprocess.py
@@ -26,18 +26,18 @@ def signal_interval(signal: np.complex64, n_samples=50000, interv=1024, Fs=12000
         
     return fft_matrix
 
-def energy_arrays(fft_matrix: np.array, n_frec_div:int) -> np.array: 
+def energy_arrays(fft_matrix: np.array, n_frec_div:int, offset=1) -> np.array: 
     '''
     Calcula la matriz de diferencias de energía de un intervalo (fila o ventana)
     con respecto al previo repartido en n_frec_div bloques (columnas) 
     '''
-    n_windows = fft_matrix.shape[0]
+    n_windows = fft_matrix.shape[0]-offset
     length_frec_div = fft_matrix.shape[1] // n_frec_div
-    energy_dif = np.zeros((n_windows-1, n_frec_div), dtype=np.float64)
+    energy_dif = np.zeros((n_windows, n_frec_div), dtype=np.float64)
     
     for i in range(n_frec_div):
         energies = np.sum(np.abs(fft_matrix[:,i*length_frec_div:(i+1)*length_frec_div])**2, axis=1)
-        energy_dif[:,i] = np.log(energies[1:]/energies[:-1])
+        energy_dif[:,i] = np.log(energies[offset:]/energies[:-offset])
         
     return np.abs(energy_dif)
 
@@ -54,17 +54,23 @@ def split_data(signal_list, train_ratio=0.8):
     
     return train_data, test_data
 
-def compute_energy_matrix_and_labels(dataset:list, n_samples:int, interv:int, n_frec_div:int, class_mapping:dict):
+def compute_energy_matrix_and_labels(dataset:list, n_samples:int, interv:int, n_frec_div:int, class_mapping:dict, anomaly_duration=12500):
     '''Builds energy arrays for each train signal 
     (x=window samples, y=frecuency divisions z=signal)'''
     array_length = (n_samples // interv) - 1
     N = len(dataset)
     
-    energy_dif_matrix = np.zeros((array_length*N, n_frec_div), dtype=np.float64)
-    sample_labels = np.zeros(array_length*N, np.int8)
+    energy_dif_matrix = np.empty((array_length*N, n_frec_div), dtype=np.float64)
+    sample_labels = np.empty(array_length*N, np.int8)
+    sample_labels[:] = class_mapping["Clean"]
     for i, signal in tqdm(enumerate(dataset)):
         energy_dif_matrix[array_length*i:array_length*(i+1),:] = energy_arrays(signal_interval(signal["Data"], n_samples, interv), n_frec_div)
-        sample_labels[i*array_length:(i*array_length)+array_length] = class_mapping[signal["Class"]]
+        #sample_labels[i*array_length:(i*array_length)+array_length] = class_mapping[signal["Class"]]
+        if signal["Class"]!="Clean":
+            start = i*array_length + (signal['JammingStartTime']//interv)
+            stop = i*array_length + (signal['JammingStartTime']+anomaly_duration)//interv + 1
+            sample_labels[start] = class_mapping[signal["Class"]]
+            sample_labels[stop] = class_mapping[signal["Class"]]
         
     return energy_dif_matrix, sample_labels
     
