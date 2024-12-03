0cbcb48dac6d9ad163854012f076882401d998fd
NotTurtle01 <omiranchez@gmail.com>
2024-12-03 09:57:15 +0100

    PCA visualization (first version)

--- demo.py
+++ demo.py
@@ -1,9 +1,11 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button, RadioButtons
 from matplotlib.mlab import window_hanning,specgram,psd
 import matplotlib.animation as animation
 from matplotlib.colors import LogNorm
+import matplotlib.lines as mlines
 import random
 
 from scipy.fft import fftshift
@@ -12,6 +14,10 @@ from scipy.signal import welch, spectrogram
 from src.rf_stream import Signal
 from src.utils_import import load_data
 from src.utils_preprocess import split_data
+
+from sklearn.decomposition import PCA
+from src.utils_preprocess import signal_interval, energy_arrays, compute_energy_matrix_and_labels
+from src.utils_clustering import create_cluster
 
 #from src.utils_exploration import plot_PSD
 #from src.rf_stream import 
@@ -32,6 +38,10 @@ rate = Fs
 
 freq = 50 # simulates sinusoid at 50 Hz
 amp = 1
+
+n_frec_div = 32
+n_samples = 50000
+num_signal_intervals = n_samples//nfft
 
 def get_sample(signal):
     """
@@ -79,44 +89,65 @@ def calc_psd2(signal,rate):
                                 Fs = rate,NFFT=nfft,noverlap=overlap)
     return Pxx,freqs
 
+def calc_pca_points(data,nfft,pca,kmeans):
+    energy_dif = energy_arrays(signal_interval(data, N, nfft), n_frec_div, offset=1)
+    new_pca_points = pca.transform(energy_dif[-1:])
+    predicted_cluster = kmeans.predict(energy_dif)
+    return pd.DataFrame({"PC1": new_pca_points[:, 0], "PC2": new_pca_points[:, 1], "cluster": predicted_cluster})
+
 def get_data_and_model(): # TODO
     # Asumption: all signals consist of 50k samples
     n_samples = 50000
     interv = 1024 # Hyperparameter 1
     array_length = (n_samples // interv) - 1
     n_frec_div = 32 # Hyperparameter 2
+    
+    class_mapping = {"Clean": 0, "Narrowband Start": 1, "Narrowband Stop": 2, "Wideband Start": 3, "Wideband Stop": 4}
 
     # Load data
     signals_clean = load_data('dataset/Jamming/Clean', 'dataset/Jamming/metadata.csv')
-    #signals_narrowband = load_data('dataset/Jamming/Narrowband', 'dataset/Jamming/metadata.csv')
-    #signals_wideband = load_data('dataset/Jamming/Wideband', 'dataset/Jamming/metadata.csv')
+    signals_narrowband = load_data('dataset/Jamming/Narrowband', 'dataset/Jamming/metadata.csv')
+    signals_wideband = load_data('dataset/Jamming/Wideband', 'dataset/Jamming/metadata.csv')
 
     # Partition train=0.8, test=0.2
     clean_train, clean_test = split_data(signals_clean, 0.8)
-    #narrowband_train, narrowband_test = split_data(signals_narrowband, 0.8)
-    #wideband_train, wideband_test = split_data(signals_wideband, 0.8)
+    narrowband_train, narrowband_test = split_data(signals_narrowband, 0.8)
+    wideband_train, wideband_test = split_data(signals_wideband, 0.8)
 
     #train = clean_train + narrowband_train + wideband_train
-    #test = clean_test + narrowband_test + wideband_test
+    test = clean_test + narrowband_test + wideband_test
 
     #random.shuffle(train)
-    #random.shuffle(test)
+    random.shuffle(test)
 
     #train_energy_dif_matrix, sample_labels = compute_energy_matrix_and_labels(train, n_samples, interv, n_frec_div, class_mapping)
+    test_energy_dif_matrix, sample_labels = compute_energy_matrix_and_labels(test, n_samples, interv, n_frec_div, class_mapping)
 
     sample = clean_test[0]["Data"]
+    
+    ###
+    # PCA MODEL for visualization with KMEANS support for model prediction
+    pca = PCA(n_components=2)
+    pca.fit(test_energy_dif_matrix)
+    kmeans = create_cluster(test_energy_dif_matrix)
+    
+    sample_signal = clean_test[0]
+    sample_train = energy_arrays(signal_interval(sample, N, nfft), n_frec_div, offset=1)
+    pca_data = pd.DataFrame(pca.transform(sample_train),columns=['PC1','PC2']) 
+    pca_data['cluster'] = pd.Categorical(kmeans.predict(sample_train))
+    ###
 
-    return sample
+    return sample, pca, pca_data, kmeans
 
 def main(SEED=1337):
     random.seed(SEED)
     np.random.seed(SEED)
 
-    sample = get_data_and_model()
+    sample, pca, pca_data, kmeans = get_data_and_model()
 
     #fig = plt.figure()
 
-    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2)
+    fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(3,2, figsize=(10,8))
     
     plt.subplots_adjust(left=0.25, bottom=0.25)
     t = np.arange(0.0, 1.0, 0.001)
@@ -193,7 +224,35 @@ def main(SEED=1337):
     ax3.set_ylabel("PSD [dB/Hz]")
     ax3.set_title("PSD")
     ax3.grid(True)
+    
+    ### 
+    # PCA visualization
+    global pca_buffer
+    pca_buffer = pd.DataFrame(columns=["PC1", "PC2", "cluster"])
+    pca_buffer["PC1"], pca_buffer["PC2"], pca_buffer["cluster"] = pca_data["PC1"], pca_data["PC2"], pca_data["cluster"]
+    scatter_alpha = np.linspace(1.0,0.1, num_signal_intervals)
+    scatter = ax5.scatter(
+        pca_data["PC1"], pca_data["PC2"],
+        c=pca_data['cluster'], 
+        alpha=scatter_alpha[:len(pca_buffer)]
+    )
+    
+    ax5.set_title('PCA')
+    ax5.set_xlabel('PC1')
+    ax5.set_ylabel('PC2')
+    ax5.set_xlim(-1,1)
+    ax5.set_ylim(-1,1)
+    ax5.grid(True)
+    
+    #legend1 = ax5.legend(*scatter.legend_elements(),
+    #               loc="upper left", title="")
+    #ax5.add_artist(legend1)
+    
+    fig.subplots_adjust(left=0.25, bottom=0.25, right=0.95) 
+    fig.delaxes(ax6)
 
+    ###
+    
     def update_fig(n):
         """
         updates the image, just adds on samples at the start until the maximum size is
@@ -202,12 +261,24 @@ def main(SEED=1337):
         inputs: iteration number
         outputs: updated image
         """
+        global pca_buffer
+        
         data = get_sample(signal)
         
         Pxx,freqs = calc_psd(data,rate)
-
+        
         psd_line.set_data(freqs,Pxx)
-
+        
+        ###
+        # PCA update 
+        new_pca_points = calc_pca_points(data, nfft, pca, kmeans)
+        pca_buffer = pd.concat([new_pca_points, pca_buffer], ignore_index=True)
+        if len(pca_buffer) > num_signal_intervals:
+            pca_buffer = pca_buffer.iloc[:num_signal_intervals]
+        scatter.set_offsets(pca_buffer[["PC1", "PC2"]].to_numpy())
+        scatter.set_alpha(scatter_alpha[:len(pca_buffer)])
+        scatter.set_array(pca_buffer["cluster"].to_numpy())
+        ###
         
         arr2D,freqs,bins = get_specgram(data,rate)
         im_data = im.get_array()
